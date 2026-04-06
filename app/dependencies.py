405a744f8e04131f7e43a1c690969b7715e046d2
nandanac100 <nandanac100@gmail.com>
2026-04-06 15:10:46 +0530

    Improved Dashboard apis by creating apis for recent-activities,monthly-trends and get category wise totals

--- app/dependencies.py
+++ app/dependencies.py
@@ -13,6 +13,6 @@ def admin_only(role:UserRole=Depends(get_current_role)):
         raise HTTPException(status_code=403,detail="Access only for admin")
     return role
 def record_view_role(role:UserRole=Depends(get_current_role)):
-    if role not in [UserRole.admin,UserRole.aanlyst]:
+    if role not in [UserRole.admin,UserRole.analyst]:
         raise HTTPException(status_code=403,detail="Access only for admin and analyst")
     return role
--- app/routers/dashboard.py
+++ app/routers/dashboard.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI,Depends,HTTPException,APIRouter
+from datetime import datetime
 from sqlalchemy.orm import Session
 from app.db import get_db
 from app.models import User,Records
@@ -20,3 +21,34 @@ def get_summary(db:Session=Depends(get_db),role:UserRole=Depends(record_view_rol
         "total_expense":net_expense,
         "net_balance":net_balance
     }
+
+@router.get("/category-totals")
+def get_catetegory_totals(db:Session=Depends(get_db)):
+    category_totals=(db.query(Records.category,func.sum(Records.amount).label("total")).group_by(Records.category).all())
+    return [
+        {
+            "category":category,
+            "total":total
+        }for category,total in category_totals
+    ]
+
+@router.get("/recent-activities")
+def get_recent_activities(db:Session=Depends(get_db)):
+    records=(
+        db.query(Records).order_by(Records.created_at.desc()).limit(5).all()
+        )
+    return records
+
+@router.get("/monthly-trends")
+def get_monthly_trends(db:Session=Depends(get_db),role:UserRole=Depends(record_view_role)):
+    monthly_trends=(
+        db.query(func.date_trunc("month",Records.date).label("month"),Records.type,func.sum(Records.amount).label("total")).group_by(func.date_trunc("month",Records.date),Records.type).order_by(func.date_trunc("month",Records.date)).all()
+    )
+
+    return [
+        {
+            "month": month.strftime("%Y-%m"),
+            "type": record_type,
+            "total":total
+        }for month,record_type,total in monthly_trends
+    ]
