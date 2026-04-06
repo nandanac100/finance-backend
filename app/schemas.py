1979e4dc45773e8c46c9d8aaaed3bd67d9151dd8
nandanac100 <nandanac100@gmail.com>
2026-04-06 08:31:22 +0530

    Implemented Pydantic schemas for data validation

---
+++ app/schemas.py
@@ -0,0 +1,44 @@
+from pydantic import BaseModel,EmailStr
+from datetime import datetime
+from typing import Optional
+from uuid import UUID
+
+class UserCreate(BaseModel):
+    name:str
+    email:EmailStr
+    password:str
+    role:str
+
+class UserResponse(BaseModel):
+    id:UUID
+    name:str
+    email:EmailStr
+    role:str
+    is_active:bool
+    created_at:datetime
+
+    class Config:
+        from_attributes=True
+
+class RecordCreate(BaseModel):
+    title:str
+    amount:float
+    type:str
+    category:str
+    description:Optional[str]=None
+    date:datetime
+    created_by:UUID
+
+class RecordResponse(BaseModel):
+    id:UUID
+    title:str
+    amount:float
+    type:str
+    category:str
+    description:Optional[str]=None
+    date:datetime
+    created_at:datetime
+    created_by:UUID
+
+    class Config:
+        from_attributes=True
