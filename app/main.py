8522bd81cb5c3f610aea31692777dbbcc5256517
nandanac100 <nandanac100@gmail.com>
2026-04-06 09:33:43 +0530

    Cnetralized Routes with APIRouter

--- app/main.py
+++ app/main.py
@@ -3,26 +3,13 @@ from sqlalchemy.orm import Session
 from app.db import Base,engine,get_db
 from app.models import User,Records
 from app.schemas import UserCreate,UserResponse
+from app.routers import users
 
 Base.metadata.create_all(bind=engine)
 app=FastAPI()
+app.include_router(users.router)
 
 @app.get("/")
 def home():
     return "hello world"
 
-@app.post("/user",response_model=UserResponse)
-def create_user(user:UserCreate,db:Session=Depends(get_db)):
-    existing_user=db.query(User).filter(User.email==user.email).first()
-    if existing_user:
-        raise HTTPException(status_code=400,detail="Email already exists")
-    new_user=User(name=user.name,email=user.email,password_hash=user.password,role=user.role)
-    db.add(new_user)
-    db.commit()
-    db.refresh(new_user) 
-    return new_user
-
-@app.get("/user",response_model=list[UserResponse])
-def get_users(db:Session=Depends(get_db)):
-    users=db.query(User).all()
-    return users
---
+++ app/routers/users.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI,Depends,HTTPException,APIRouter
+from sqlalchemy.orm import Session
+from app.db import Base,engine,get_db
+from app.models import User,Records
+from app.schemas import UserCreate,UserResponse
+
+router=APIRouter(prefix="/users" ,tags=["Users"])
+
+@router.post("/user",response_model=UserResponse)
+def create_user(user:UserCreate,db:Session=Depends(get_db)):
+    existing_user=db.query(User).filter(User.email==user.email).first()
+    if existing_user:
+        raise HTTPException(status_code=400,detail="Email already exists")
+    new_user=User(name=user.name,email=user.email,password_hash=user.password,role=user.role)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user) 
+    return new_user
+
+@router.get("/user",response_model=list[UserResponse])
+def get_users(db:Session=Depends(get_db)):
+    users=db.query(User).all()
+    return users
+
